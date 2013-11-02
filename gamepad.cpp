370b09f40f01e8fd97c95710e6dc8907fa55cd16
agashka <agashka2@gmail.com>
2013-11-01 21:16:31 -0300

    bug fixes; 10ms debounce

--- gamepad.cpp
+++ gamepad.cpp
@@ -14,15 +14,15 @@
 * -------------------------------------------------------------------------
 */
 
-const int PIN_BUT_A = 2;
-const int PIN_BUT_B = 3;
-const int PIN_BUT_SELECT = 4;
-const int PIN_BUT_START = 5;
+const int PIN_BUT_A = 8;
+const int PIN_BUT_B = 9;
+const int PIN_BUT_SELECT = 6;
+const int PIN_BUT_START = 7;
 
-const int PIN_BUT_UP = 6;
-const int PIN_BUT_DOWN = 7;
-const int PIN_BUT_LEFT = 8;
-const int PIN_BUT_RIGHT = 9;
+const int PIN_BUT_UP = 5;
+const int PIN_BUT_DOWN = 3;
+const int PIN_BUT_LEFT = 2;
+const int PIN_BUT_RIGHT = 4;
 
 
 class Button {
@@ -88,37 +88,52 @@ void setup() {
 }
 
 void loop() {
+  boolean anyChanged = false;
+  
   if(up->hasChanged()) {
-    joySt.yAxis = up->isPressed() ? 255 : 127;
+    joySt.yAxis = down->isPressed() ?  0 : 127;
+    anyChanged = true;
   }
 
   if(down->hasChanged()) {
-    joySt.yAxis = down->isPressed() ?  0 : 127;
+    joySt.yAxis = up->isPressed() ? 255 : 127;
+    anyChanged = true;
   }
 
   if(left->hasChanged()) {
     joySt.xAxis = left->isPressed() ? 0 : 127;
+    anyChanged = true;
   }
 
   if(right->hasChanged()) {
     joySt.xAxis = right->isPressed() ? 255 : 127;
+    anyChanged = true;
   }
 
   if(a->hasChanged()) {
     joySt.buttons = a->isPressed() ? (joySt.buttons | 1) : (joySt.buttons & 254);
+    anyChanged = true;
   }
 
   if(b->hasChanged()) {
     joySt.buttons = b->isPressed() ? (joySt.buttons | 2) : (joySt.buttons & 253);
+    anyChanged = true;
   }
 
   if(start->hasChanged()) {
-
+    joySt.buttons = start->isPressed() ? (joySt.buttons | 4) : (joySt.buttons & 251);
+    anyChanged = true;
   }
 
   if(select->hasChanged()) {
-
+    joySt.buttons = select->isPressed() ? (joySt.buttons | 8) : (joySt.buttons & 247);
+    anyChanged = true;
+  }
+  
+  if(anyChanged) {
+    anyChanged = false;
+    Joystick.setState(&joySt);
+    delay(10);
   }
 
-  Joystick.setState(&joySt);
 }
